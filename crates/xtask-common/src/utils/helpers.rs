7862572eea1343b4ee3fec21be7b31432fb3d95d
syl20bnr <sylvain.benner@gmail.com>
2024-08-01 12:11:47 -0400

    Remove distinction between unit and integration add tests helper

--- crates/xtask-common/src/utils/helpers.rs
+++ crates/xtask-common/src/utils/helpers.rs
@@ -5,7 +5,7 @@ use crate::{endgroup, group, utils::process::run_process};
 /// Allow to build additional crates outside the common build commands
 pub fn additional_crates_build(crates: Vec<&str>, params: Vec<&str>) -> anyhow::Result<()> {
     let params_display = params.join(", ");
-    let mut base_args = vec!["build"];
+    let mut base_args = vec!["build", "--color", "always"];
     base_args.extend(params);
     crates.iter().try_for_each(|c| {
         group!("Build: {} (with params: {})", *c, params_display);
@@ -17,46 +17,19 @@ pub fn additional_crates_build(crates: Vec<&str>, params: Vec<&str>) -> anyhow::
     })
 }
 
-/// Allow to unit test additional crates outside the common unit-tests commands
-pub fn additional_crates_unit_tests(crates: Vec<&str>, params: Vec<&str>) -> anyhow::Result<()> {
+/// Allow to test additional crates with specific flags and config
+pub fn additional_crates_tests(crates: Vec<&str>, params: Vec<&str>) -> anyhow::Result<()> {
     let params_display = params.join(", ");
-    let mut base_args = vec!["test"];
+    let mut base_args = vec!["test", "--color", "always"];
     base_args.extend(params);
     crates.iter().try_for_each(|c| {
-        group!("Unit Tests: {} (with params: {})", *c, params_display);
+        group!("Additional Tests: {} (with params: {})", *c, params_display);
         let mut args = base_args.clone();
         args.extend(vec!["-p", *c]);
         run_process(
             "cargo",
             &args,
-            &format!("Unit test failed for {}", *c),
-            true,
-        )?;
-        endgroup!();
-        Ok(())
-    })
-}
-
-/// Allow to integration test additional crates outside the common integration-tests commands
-pub fn additional_crates_integration_tests(
-    crates: Vec<&str>,
-    params: Vec<&str>,
-) -> anyhow::Result<()> {
-    let params_display = params.join(", ");
-    let mut base_args = vec!["test", "--test", "test_*"];
-    base_args.extend(params);
-    crates.iter().try_for_each(|c| {
-        group!(
-            "Integration Tests: {} (with params: {})",
-            *c,
-            params_display
-        );
-        let mut args = base_args.clone();
-        args.extend(vec!["-p", *c]);
-        run_process(
-            "cargo",
-            &args,
-            &format!("Integration test failed for {}", *c),
+            &format!("Additional test failed for {}", *c),
             true,
         )?;
         endgroup!();
@@ -67,7 +40,7 @@ pub fn additional_crates_integration_tests(
 /// Allow to build crate documentation additional crates outside the common doc commands
 pub fn additional_crates_doc_build(crates: Vec<&str>, params: Vec<&str>) -> anyhow::Result<()> {
     let params_display = params.join(", ");
-    let mut base_args = vec!["doc", "--no-deps"];
+    let mut base_args = vec!["doc", "--no-deps", "--color", "always"];
     base_args.extend(params);
     crates.iter().try_for_each(|c| {
         group!("Doc Build: {} (with params: {})", *c, params_display);
