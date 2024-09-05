e5f2817e11c42a191d42372825c1ad69e15f665e
syl20bnr <sylvain.benner@gmail.com>
2024-09-04 23:12:46 -0400

    Output cargo workspace process to stdout only

--- crates/tracel-xtask/src/logging.rs
+++ crates/tracel-xtask/src/logging.rs
@@ -48,7 +48,7 @@ macro_rules! group {
     ($($arg:tt)*) => {
         let title = format!($($arg)*);
         if std::env::var("CI").is_ok() {
-            eprintln!("::group::{}", title)
+            println!("::group::{}", title)
         } else {
             log!(log::Level::Info, "{}", title)
         }
@@ -61,7 +61,7 @@ macro_rules! group_info {
     ($($arg:tt)*) => {
         let title = format!($($arg)*);
         if std::env::var("CI").is_ok() {
-            eprintln!("{}", title)
+            println!("{}", title)
         } else {
             log!(log::Level::Info, "{}", title)
         }
@@ -74,7 +74,7 @@ macro_rules! endgroup {
     // endgroup!()
     () => {
         if std::env::var("CI").is_ok() {
-            eprintln!("::endgroup::")
+            println!("::endgroup::")
         }
     };
 }
--- crates/tracel-xtask/src/utils/process.rs
+++ crates/tracel-xtask/src/utils/process.rs
@@ -111,7 +111,7 @@ pub fn run_process_for_workspace<'a>(
     // Process the stdout to inject log groups
     let mut ignore_error = false;
     let mut close_group = false;
-    for (line, is_stderr) in rx.iter() {
+    for (line, _is_stderr) in rx.iter() {
         let mut skip_line = false;
 
         if let Some(rx) = &group_rx {
@@ -137,11 +137,7 @@ pub fn run_process_for_workspace<'a>(
         }
 
         if !skip_line {
-            if is_stderr {
-                eprintln!("{}", line);
-            } else {
-                println!("{}", line);
-            }
+            println!("{}", line);
         }
     }
 
