876ed93c1050081381ed033375fa43db831d5b68
Genna Wingert <1058083+wingertge@users.noreply.github.com>
2024-10-30 17:19:07 +0100

    Add helper for custom checks (#7)

--- crates/tracel-xtask/src/utils/helpers.rs
+++ crates/tracel-xtask/src/utils/helpers.rs
@@ -30,6 +30,32 @@ pub fn custom_crates_build(
     })
 }
 
+/// Allow to check additional crates outside the common check commands
+pub fn custom_crates_check(
+    crates: Vec<&str>,
+    args: Vec<&str>,
+    envs: Option<HashMap<&str, &str>>,
+    path: Option<&Path>,
+    group_msg: &str,
+) -> anyhow::Result<()> {
+    let mut base_args = vec!["check", "--color", "always"];
+    base_args.extend(args);
+    crates.iter().try_for_each(|c| {
+        group!("Custom Check: {} ({})", *c, group_msg);
+        let mut args = base_args.clone();
+        args.extend(vec!["-p", *c]);
+        run_process(
+            "cargo",
+            &args,
+            envs.clone(),
+            path,
+            &format!("Custom check failed for {}", *c),
+        )?;
+        endgroup!();
+        Ok(())
+    })
+}
+
 /// Allow to test additional crates with specific flags and config
 pub fn custom_crates_tests(
     crates: Vec<&str>,
