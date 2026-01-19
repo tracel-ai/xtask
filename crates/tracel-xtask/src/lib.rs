e988fa3443de43eb667b2bfdeed743b0daaa5dd1
syl20bnr <sylvain.benner@tracel.ai>
2026-01-18 22:17:16 -0500

    feat: re-export utils as base_utils

--- crates/tracel-xtask/src/lib.rs
+++ crates/tracel-xtask/src/lib.rs
@@ -67,11 +67,13 @@ pub mod prelude {
     pub use crate::init_xtask;
     pub use crate::parse_args;
     pub use crate::register_cleanup;
+    pub use crate::utils as base_utils;
     pub use crate::utils::aws;
     pub use crate::utils::cargo::ensure_cargo_crate_is_installed;
     pub use crate::utils::cleanup::CLEANUP_HANDLER;
     pub use crate::utils::git;
     pub use crate::utils::helpers;
+    pub use crate::utils::process;
     pub use crate::utils::process::random_port;
     pub use crate::utils::process::run_process;
     pub use crate::utils::process::run_process_for_package;
