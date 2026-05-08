5423410b396b23f2759eeeeeaba60035ea052af1
syl20bnr <sylvain.benner@tracel.ai>
2026-05-08 11:27:04 -0400

    feat: re-export Infra sub command args structs

--- crates/tracel-xtask/src/lib.rs
+++ crates/tracel-xtask/src/lib.rs
@@ -66,7 +66,10 @@ pub mod prelude {
     pub use crate::commands::image::ImageRollbackSubCmdArgs;
     pub use crate::commands::image::ImageRolloutSubCmdArgs;
     pub use crate::commands::image::ImageSubCommand;
+    pub use crate::commands::infra::InfraApplySubCmdArgs;
     pub use crate::commands::infra::InfraCmdArgs;
+    pub use crate::commands::infra::InfraOutputSubCmdArgs;
+    pub use crate::commands::infra::InfraProvidersSubCmdArgs;
     pub use crate::commands::infra::InfraSubCommand;
     pub use crate::commands::publish::PublishCmdArgs;
     pub use crate::commands::secrets::SecretsCmdArgs;
